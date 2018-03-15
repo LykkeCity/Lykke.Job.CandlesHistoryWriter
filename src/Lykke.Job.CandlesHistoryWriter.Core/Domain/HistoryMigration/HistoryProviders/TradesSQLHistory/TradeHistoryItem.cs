166be8b65e1bfbb5ea63047f4635ddbab9b9239a
Dmitry Smolyakov <dmitry.smolyakov@lykke.com>
2018-03-15 18:16:42 +0300

    Initial commit: temporary/intermediate results.

---
+++ src/Lykke.Job.CandlesHistoryWriter.Core/Domain/HistoryMigration/HistoryProviders/TradesSQLHistory/TradeHistoryItem.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lykke.Job.CandlesHistoryWriter.Core.Domain.HistoryMigration.HistoryProviders.TradesSQLHistory
+{
+    public class TradeHistoryItem
+    {
+        public decimal Price { get; set; }
+        public decimal Volume { get; set; }
+        public decimal OppositeVolume { get; set; }
+        public DateTime DateTime { get; set; }
+    }
+}
