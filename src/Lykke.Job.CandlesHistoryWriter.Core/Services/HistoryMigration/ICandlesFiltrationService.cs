c8a1185fe403e023dc2accf7afcc32ced82452da
Dmitry Smolyakov <dmitry.smolyakov@lykke.com>
2018-04-03 20:56:21 +0300

    LWDEV-6012 Refactored.

---
+++ src/Lykke.Job.CandlesHistoryWriter.Core/Services/HistoryMigration/ICandlesFiltrationService.cs
@@ -0,0 +1,15 @@
+﻿using Lykke.Job.CandlesHistoryWriter.Core.Domain.Candles;
+using Lykke.Job.CandlesProducer.Contract;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lykke.Job.CandlesHistoryWriter.Core.Services.HistoryMigration
+{
+    public interface ICandlesFiltrationService
+    {
+        Task<IReadOnlyList<ICandle>> TryGetExtremeCandlesAsync(string assetPairId, CandlePriceType priceType, double limitLow, double limitHigh);
+        Task<(int deletedCandlesCount, int replacedCandlesCount)> FixExtremeCandlesAsync(IReadOnlyList<ICandle> extremeCandles, CandlePriceType priceType);
+    }
+}
--- src/Lykke.Job.CandlesHistoryWriter/Models/Filtration/CandlesFiltrationRequestModel.cs
+++ src/Lykke.Job.CandlesHistoryWriter/Models/Filtration/CandlesFiltrationRequestModel.cs
@@ -1,11 +1,30 @@
 ﻿using Lykke.Job.CandlesHistoryWriter.Core.Domain.HistoryMigration.Filtration;
+using System.Collections.Generic;
 
 namespace Lykke.Job.CandlesHistoryWriter.Models.Filtration
 {
     public class CandlesFiltrationRequestModel : ICandlesFiltrationRequest
     {
-        public string AssetId { get; set; }
+        public string AssetPairId { get; set; }
         public double LimitLow { get; set; }
         public double LimitHigh { get; set; }
+
+        public static IReadOnlyDictionary<string, string> CheckupModel(CandlesFiltrationRequestModel request)
+        {
+            var result = new Dictionary<string, string>();
+
+            if (request == null)
+                result.Add("Request", "The request can not be null.");
+            else
+            {
+                if (string.IsNullOrWhiteSpace(request.AssetPairId))
+                    result.Add("AssetPairId", "Can not be null or empty.");
+
+                if (request.LimitHigh < request.LimitLow)
+                    result.Add("Limits", "LimitLow must be less than LimitHigh.");
+            }
+
+            return result;
+        }
     }
 }
