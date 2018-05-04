89b74df88b2725382f5732c5a853ac1075ebbb03
Dmitry Smolyakov <dmitry.smolyakov@lykke.com>
2018-05-04 19:27:22 +0300

    After merge from LWDEV-6920 commit 2.

--- src/Lykke.Job.CandlesHistoryWriter.Core/Domain/Candles/ICandlesHistoryRepository.cs
+++ src/Lykke.Job.CandlesHistoryWriter.Core/Domain/Candles/ICandlesHistoryRepository.cs
@@ -12,7 +12,6 @@ namespace Lykke.Job.CandlesHistoryWriter.Core.Domain.Candles
         bool CanStoreAssetPair(string assetPairId);
         Task<ICandle> TryGetFirstCandleAsync(string assetPairId, CandleTimeInterval interval, CandlePriceType priceType);
         IReadOnlyList<string> GetStoredAssetPairs();
-        Task<bool> DeleteCandlesAsync(string assetPairId, CandleTimeInterval interval, CandlePriceType priceType, DateTime? from = null, DateTime? to = null);
         Task<int> DeleteCandlesAsync(IReadOnlyList<ICandle> candlesToDelete);
         Task<int> ReplaceCandlesAsync(IReadOnlyList<ICandle> candlesToReplace);
         Task<bool> DeleteCandlesAsync(string assetPairId, CandleTimeInterval interval, CandlePriceType priceType, DateTime? from = null, DateTime? to = null);
