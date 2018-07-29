d55bad813293b34a217ba9738c2b8965608fe46c
Dmitry Smolyakov <dmitry.smolyakov@lykke.com>
2018-07-29 03:34:11 +0300

    LWDEV-8682 Attempt to eliminate race conditions around cache.

---
+++ src/Lykke.Job.CandlesHistoryWriter.Services/Candles/CandlesCacheSemaphore.cs
@@ -0,0 +1,29 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using Lykke.Job.CandlesHistoryWriter.Core.Services.Candles;
+
+namespace Lykke.Job.CandlesHistoryWriter.Services.Candles
+{
+    /// <summary>
+    /// The process-global blocker for different cache operations. Please, use it as a singleton service in IOC.
+    /// </summary>
+    public class CandlesCacheSemaphore : ICandlesCacheSemaphore
+    {
+        private readonly SemaphoreSlim _sem; // Not static 'casuse the preffered way of using the class is to register it as a singleton at startup
+
+        public CandlesCacheSemaphore()
+        {
+            _sem = new SemaphoreSlim(1, 1);
+        }
+
+        public void Release()
+        {
+            _sem.Release();
+        }
+
+        public async Task WaitAsync()
+        {
+            await _sem.WaitAsync();
+        }
+    }
+}
