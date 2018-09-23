ef5bd7543de985a162eae1be63eb26964115aeb7
Dmitry Smolyakov <dmitry.smolyakov@lykke.com>
2018-09-23 22:42:06 +0300

    LWDEV-9392 Cache all asset pairs (not only enabled).

--- src/Lykke.Job.CandlesHistoryWriter.Services/Assets/AssetPairsManager.cs
+++ src/Lykke.Job.CandlesHistoryWriter.Services/Assets/AssetPairsManager.cs
@@ -47,5 +47,15 @@ namespace Lykke.Job.CandlesHistoryWriter.Services.Assets
                     (exception, timespan) => _log.WriteErrorAsync("Get all asset pairs with retry", string.Empty, exception))
                 .ExecuteAsync(async () => (await _apiService.GetAllAssetPairsAsync()).Where(a => !a.IsDisabled));
         }
+
+        public Task<IEnumerable<AssetPair>> GetAllAsync()
+        {
+            return Policy
+                .Handle<Exception>()
+                .WaitAndRetryForeverAsync(
+                    retryAttempt => TimeSpan.FromSeconds(Math.Pow(2, retryAttempt)),
+                    (exception, timespan) => _log.WriteErrorAsync("Get all asset pairs with retry", string.Empty, exception))
+                .ExecuteAsync(async () => (await _apiService.GetAllAssetPairsAsync()).AsEnumerable());
+        }
     }
 }
